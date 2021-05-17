83c659f21b69b606e1598f776fa3573ddb2fa3a6
Justin Sin <48116685+jvil26@users.noreply.github.com>
2021-05-16 18:01:39 -0700

    Spell Corrector, Page Navigation

--- cheta-extension/build/switch.js
+++ cheta-extension/build/switch.js
@@ -16,6 +16,8 @@ function init() {
     console.log("Error loading ChETA: Input not found")
     return;
   }
+  speller.train(config.trainData);
+  speller.train(config.trainData);
   var text = inputs[0].value.split(config.code).reverse()[0];
   var dictionary = config.dictionary;
   var misspelledWords = findMisspelledWords(text, dictionary);
@@ -82,23 +84,50 @@ function init() {
     selectedTextSpan.textContent = ""+this.selectedText;
   });
 
-  const div = document.createElement('div');
-  var finalHTML = '<div id="cheta-flt-dv"><p class="cheta-flt-p">ChETA</p><br>';
-  finalHTML += '<p class="cheta-pfnt">Words: <span id="cheta-data-wordcount">'+text.split(" ").length+'</span></p>';
-  finalHTML += '<p class="cheta-pfnt">Chars: <span id="cheta-data-charcount">'+text.length+'</span></p><br><hr>';
-  finalHTML += '<p class="cheta-pfnt">Reading Score: <span id="cheta-data-score">'+score+'</span></p><br>';
-  finalHTML += '<p class="cheta-pfnt">School Level (US): <span id="cheta-data-schoolLevel">'+schoolLevel+'</span></p><br>';
-  finalHTML += '<p class="cheta-pfnt">Note: <span id="cheta-data-scoreNote">'+scoreNote+'</span></p><br><hr>';
-  finalHTML += '<p class="cheta-pfnt">Selected Text: <span id="cheta-data-selectedText"></span></p><br><hr>';
-  finalHTML += '<p class="cheta-pfnt">Misspelled Words: <span id="cheta-data-mispelledWords">';
-  for (let i = 0; i < misspelledWords.length; i++) {
-    finalHTML += '<p class="mispelledWords">'+misspelledWords[i]+'</p>'
-  }
-  finalHTML += '</span></p>';
-  finalHTML += '</div>';
-  div.innerHTML = finalHTML;
-  document.body.appendChild(div);
+  var link = document.createElement('link');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  link.href = 'https://use.fontawesome.com/releases/v5.8.2/css/all.css';
+  document.head.appendChild(link);
+
+
+  const backButtonHtml = '<span><i class="fas fa-chevron-left" id="backButton" style="position: absolute; top: 10px; margin: 10px, 0px, 25px 5px; left: 5px"></i></span>';
+
+  const homePage = document.createElement('div');
+    var finalHTML = '<div id="cheta-flt-dv"><p class="cheta-flt-p">ChETA</p><br>';
+      finalHTML += '<p class="cheta-pfnt">Words: <span id="cheta-data-wordcount">'+text.split(" ").length+'</span></p>';
+      finalHTML += '<p class="cheta-pfnt">Chars: <span id="cheta-data-charcount">'+text.length+'</span></p><br><hr>';
+      finalHTML += '<p class="cheta-pfnt">Reading Score: <span id="cheta-data-score">'+score+'</span></p><br>';
+      finalHTML += '<p class="cheta-pfnt">School Level (US): <span id="cheta-data-schoolLevel">'+schoolLevel+'</span></p><br>';
+      finalHTML += '<p class="cheta-pfnt">Note: <span id="cheta-data-scoreNote">'+scoreNote+'</span></p><br><hr>';
+      finalHTML += '<p class="cheta-pfnt">Selected Text: <span id="cheta-data-selectedText"></span></p><br><hr>';
+      finalHTML += '<button class="cheta-pfnt" id="misspelledWords">Misspelled Words</button>';
+      finalHTML += '</div>';
+  homePage.innerHTML = finalHTML;
+  document.body.appendChild(homePage);
+
+  const misspelled_words_page = document.createElement('div');
+    var fHtml = '<div style="visibility: hidden" id="cheta-misspelledWords-dv"><p class="cheta-flt-p" style="margin-top: 25px; margin-bottom: 0">Misspelled Words</p><br>';
+      fHtml += backButtonHtml;
+      misspelledWords.forEach(obj => (
+        fHtml += '<p>'+obj.word+'</p>',
+        fHtml += '<p class="cheta-pfnt">Did you mean: <span id="cheta-data-misspelledWord">'+obj.candidate+'</span></p><hr>'
+      ));
+  misspelled_words_page.innerHTML = fHtml;
+  document.body.appendChild(misspelled_words_page);
+
+  document.getElementById('misspelledWords').onclick = () => {
+    document.getElementById('cheta-flt-dv').style.visibility = 'hidden';
+    document.getElementById('cheta-misspelledWords-dv').style.visibility = 'visible';
+  };
+
+  document.getElementById('backButton').onclick = () => {
+    document.getElementById('cheta-flt-dv').style.visibility = 'visible';
+    document.getElementById('cheta-misspelledWords-dv').style.visibility = 'hidden';
+  };
+
 }
+
 
 function getInputsByValue(value)
 {
@@ -189,16 +218,103 @@ function getSelectedText() {
 }
 
 function findMisspelledWords(text, dictionary) {
+  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
   var words = text.split(" ");
   var misspelledWords = [];
+  var obj = { word: '', candidate: '' };
+  var count = 0;
   for (let i = 0; i < words.length; i++) {
     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-    var property = words[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
+    var property = words[i];
     if (!dictionary[property]) {
-      misspelledWords.push(words[i]);
+      obj.word = words[i];
+      obj.candidate = speller.correct(words[i]);
+      misspelledWords[count] = { ...obj };
+      count++;
     }
   }
   return misspelledWords;
 }
+
+var speller = {};
+
+// Dummy initializer for non-ServerJS environments.
+var exports;
+if (!exports) exports = {};
+
+// A function that trains the language model with the words in the supplied text.
+// Multiple invocation of this function can extend the training of the model.
+exports.train = speller.train = function (text) {
+	var word, m, r = /[a-z]+/g;
+	text = text.toLowerCase();
+	while ((m = r.exec(text))) {
+		word = m[0];
+		speller.nWords[word] = speller.nWords.hasOwnProperty(word) ? speller.nWords[word] + 1 : 1;
+	}
+};
+
+// A function that returns the correction for the specified word.
+exports.correct = speller.correct = function (word) {
+	if (speller.nWords.hasOwnProperty(word)) return word;
+	var candidates = {}, list = speller.edits(word);
+	list.forEach(function (edit) {
+		if (speller.nWords.hasOwnProperty(edit)) candidates[speller.nWords[edit]] = edit;
+	});
+	if (speller.countKeys(candidates) > 0) return candidates[speller.max(candidates)];
+	list.forEach(function (edit) {
+		speller.edits(edit).forEach(function (w) {
+			if (speller.nWords.hasOwnProperty(w)) candidates[speller.nWords[w]] = w;
+		});
+	});
+	return speller.countKeys(candidates) > 0 ? candidates[speller.max(candidates)] : word;
+};
+
+// A map of words to the number of times they were encountered during training.
+// This is exported only for the benefit of spelltest.js.
+exports.nWords = speller.nWords = {};
+
+// A helper function that counts the keys in the supplied object.
+speller.countKeys = function (object) {
+	var attr, count = 0;
+	for (attr in object)
+		if (object.hasOwnProperty(attr))
+			count++;
+	return count;	
+};
+
+// A helper function that returns the word with the most occurences in the language
+// model, among the supplied candidates.
+speller.max = function (candidates) {
+	var candidate, arr = [];
+	for (candidate in candidates)
+		if (candidates.hasOwnProperty(candidate))
+			arr.push(candidate);
+	return Math.max.apply(null, arr);
+};
+
+speller.letters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// A function that returns the set of possible corrections of the specified word.
+// The edits can be deletions, insertions, alterations or transpositions.
+speller.edits = function (word) {
+	var i, results = [];
+	// deletion
+	for (i=0; i < word.length; i++)
+	    results.push(word.slice(0, i) + word.slice(i+1));
+	// transposition
+	for (i=0; i < word.length-1; i++)
+	    results.push(word.slice(0, i) + word.slice(i+1, i+2) + word.slice(i, i+1) + word.slice(i+2));
+	// alteration
+	for (i=0; i < word.length; i++)
+	    speller.letters.forEach(function (l) {
+	        results.push(word.slice(0, i) + l + word.slice(i+1));
+		});
+	// insertion
+	for (i=0; i <= word.length; i++)
+	    speller.letters.forEach(function (l) {
+	        results.push(word.slice(0, i) + l + word.slice(i));
+		});
+	return results;
+};
 
 init();
--- cheta-extension/src/Home.js
+++ cheta-extension/src/Home.js
@@ -1,6 +1,7 @@
 /*global chrome*/
 import React, { Component } from 'react';
-import dict from './dict.json'
+import dict from './dict.json';
+import trainData from './big.js';
 
 class Home extends Component {
   constructor(props){
@@ -8,6 +9,7 @@ class Home extends Component {
     this.state = {
       uniqueCode: `<Guber>`,
       dictionary: dict,
+      trainData: trainData,
       inputs: {
         priceperword: '0.20'
       }
@@ -17,13 +19,15 @@ class Home extends Component {
   nextTapped_det = () => {
     var config = {
       code: this.state.uniqueCode,
-      dictionary: this.state.dictionary
+      dictionary: this.state.dictionary,
+      trainData: this.state.trainData
     };
+
     if(this.state.inputs.priceperword > 0) {
       config['priceperword'] = this.state.inputs.priceperword;
     }
 
-    var css = "@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap'); #cheta-flt-dv { padding: 8px; z-index: 999; position: fixed; width: 140px; bottom: 10%; right: 40px; background-color: #B0FF8B; color: black; border-radius: 20px; text-align: center; box-shadow: 2px 2px 3px #999; } .cheta-flt-p { margin: 2px; font-family: 'Special Elite'; font-size: 22px; } .cheta-pfnt { margin: 2px; font-family: 'Special Elite'; font-size: 14px; }";
+    var css = "@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap'); #cheta-flt-dv { padding: 8px; z-index: 999; position: fixed; width: 140px; bottom: 10%; right: 40px; background-color: #B0FF8B; color: black; border-radius: 20px; text-align: center; box-shadow: 2px 2px 3px #999; } #cheta-misspelledWords-dv { padding: 8px; z-index: 999; position: fixed; width: 140px; bottom: 10%; right: 40px; background-color: #B0FF8B; color: black; border-radius: 20px; text-align: center; box-shadow: 2px 2px 3px #999; } .cheta-flt-p { font-family: 'Special Elite'; font-size: 22px; } .cheta-pfnt { margin: 2px; font-family: 'Special Elite'; font-size: 14px; }";
     chrome.tabs.insertCSS({code: css});
 
     chrome.tabs.executeScript({
@@ -58,6 +62,7 @@ class Home extends Component {
         </div>
         <div className="contentBox">
           <div className="no-tf-dtctd">
+            
             <p>To start analysing a textfield, <span className="resalted">follow the steps</span>:</p>
             <p>1. <span className="resalted">Locate the field</span> you want to analyze</p>
             <p>2. Replace or <span className="resalted">add</span> anywhere on the field <span className="resalted">the following code</span>:</p>
